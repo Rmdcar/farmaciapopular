48def217e1e12ca8998c4acc566b8845adabbf1a
unknown <moraisdecarvalho@gmail.com>
2025-02-18 11:41:19 -0300

    alterações

--- src/app/estados/[estado]/cidades/[cidade]/bairros/[bairro]/farmacias/[farmacia]/page.tsx
+++ src/app/estados/[estado]/cidades/[cidade]/bairros/[bairro]/farmacias/[farmacia]/page.tsx
@@ -2,10 +2,12 @@
 
 import { useParams } from 'next/navigation';
 import dados from '@/data/dados.json';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
 
 interface Dado {
-  Id: string; // Adicionado
+  Id: string;
   UF: string;
   MUNICÍPIO: string;
   CNPJ: string;
@@ -15,12 +17,28 @@ interface Dado {
   "Data do Credenciamento": string;
 }
 
+interface ReceitaData {
+  cnpj: string;
+  nome_fantasia: string;
+  descricao_tipo_de_logradouro: string;
+  logradouro: string;
+  numero: string;
+  complemento: string;
+  bairro: string;
+  ddd_telefone_1: string;
+}
+
 export default function FarmaciaPage() {
   const params = useParams();
   const estado = params?.estado;
   const cidade = typeof params?.cidade === 'string' ? params.cidade.replace(/_/g, ' ') : '';
   const bairro = typeof params?.bairro === 'string' ? decodeURIComponent(params.bairro.replace(/_/g, ' ')) : '';
   const farmacia = typeof params?.farmacia === 'string' ? decodeURIComponent(params.farmacia.replace(/_/g, ' ')) : '';
+
+  
+
+
+  const [receitaData, setReceitaData] = useState<ReceitaData[]>([]);
 
   // Verifique se os parâmetros estão definidos e são strings
   if (!estado || typeof estado !== 'string') {
@@ -42,35 +60,73 @@ export default function FarmaciaPage() {
   const filteredData = (dados as Dado[]).filter(dado => 
     dado.UF === estado && 
     dado.MUNICÍPIO === cidade && 
-    dado.BAIRRO === bairro
+    dado.BAIRRO === bairro &&
+    dado.FARMÁCIA === farmacia
   );
+  const cnpjs = filteredData.map(dado => dado.CNPJ);
+
+  // Função para formatar CNPJ
+  const formatarCNPJ = (cnpj: string) => {
+    return cnpj.replace(/^(\d{2})(\d{3})(\d{3})(\d{4})(\d{2})$/, '$1.$2.$3/$4-$5');
+  };
+
+  // Função para formatar telefone
+  const formatarTelefone = (telefone: string) => {
+    if (telefone.length === 10) {
+      return telefone.replace(/^(\d{2})(\d{4})(\d{4})$/, '($1) $2-$3');
+    } else if (telefone.length === 11) {
+      return telefone.replace(/^(\d{2})(\d{5})(\d{4})$/, '($1) $2-$3');
+    }
+    return telefone; // Retorna o telefone sem formatação se não for válido
+  };
+
+  // Função para buscar os dados da API Minha Receita
+  const fetchReceitaData = async (cnpj: string) => {
+    try {
+      const response = await axios.get(`https://minhareceita.org/${cnpj}`);
+      return response.data;
+    } catch (error) {
+      console.error(`Erro ao buscar dados para o CNPJ ${cnpj}:`, error);
+      return null;
+    }
+  };
+
+  useEffect(() => {
+    const fetchAllData = async () => {
+      const results = await Promise.all(cnpjs.map(cnpj => fetchReceitaData(cnpj)));
+      setReceitaData(results.filter(data => data !== null)); // Filtra resultados nulos
+    };
+
+    fetchAllData();
+  }, [cnpjs]);
 
   if (filteredData.length === 0) {
-    return <p>Nenhuma farmácia encontrada para os critérios fornecidos</p>;
+    return <p>Carregando...</p>;
   }
 
   return (
     <main>
-      <h1>{farmacia}</h1>
+      <h1>{receitaData[0]?.nome_fantasia || farmacia}</h1>
       <ul>
-        {filteredData.map((data) => {
-          const urlReceita = `https://solucoes.receita.fazenda.gov.br/servicos/cnpjreva/cnpjreva_solicitacao.asp?cnpj=${data.CNPJ}`;
-          const enderecoCompleto = `${data.ENDEREÇO}, ${data.BAIRRO}, ${cidade}, ${estado}, ${farmacia}`;
+        {receitaData.map((data, index) => {
+          const urlReceita = `https://solucoes.receita.fazenda.gov.br/servicos/cnpjreva/cnpjreva_solicitacao.asp?cnpj=${data.cnpj}`;
+          const enderecoCompleto = `${data.descricao_tipo_de_logradouro}, ${data.logradouro}, ${data.numero}, ${data.bairro}, ${cidade}, ${estado}, ${data.nome_fantasia}`;
           const googleMapsUrl = `https://www.google.com/maps/search/?api=1&query=${encodeURIComponent(enderecoCompleto)}`;
 
           return (
-            <li key={data.Id} className="btnPharmacies">
+            <li key={index} className="btnPharmacies">
               <p>
                 <strong>CNPJ:</strong> 
                 <a href={urlReceita} target="_blank" rel="noopener noreferrer" title="Ver CNPJ na Receita Federal">
-                  {data.CNPJ} &#x1F50D;
+                  {formatarCNPJ(data.cnpj)} &#x1F50D;
                 </a>
               </p>
-              <p><strong>Endereço:</strong> {data.ENDEREÇO}</p>
-              <p><strong>Bairro:</strong> {data.BAIRRO}</p>
-              <p><strong>Data do Credenciamento:</strong> {data["Data do Credenciamento"]}</p>
+              <p><strong>Endereço:</strong> {data.descricao_tipo_de_logradouro} {data.logradouro}, {data.numero} {data.complemento}</p>
+              <p><strong>Bairro:</strong> {data.bairro}</p>
+              <p><strong>Telefone:</strong> {formatarTelefone(data.ddd_telefone_1)}</p>
               <div>
                 <a href={googleMapsUrl} target="_blank" rel="noopener noreferrer" title="Ver no Google Maps">
+                  <br />
                   Ver no Google Maps &#x2934;&#xFE0F;
                 </a>
               </div>
@@ -78,6 +134,9 @@ export default function FarmaciaPage() {
           );
         })}
       </ul>
+ 
+
+
     </main>
   );
 }
--- src/app/estados/[estado]/cidades/[cidade]/bairros/[bairro]/page.tsx
+++ src/app/estados/[estado]/cidades/[cidade]/bairros/[bairro]/page.tsx
@@ -3,6 +3,8 @@
 import { useParams } from 'next/navigation';
 import dados from '@/data/dados.json';
 import Link from 'next/link';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 interface Dado {
   UF: string;
@@ -14,11 +16,24 @@ interface Dado {
   "Data do Credenciamento": string;
 }
 
+interface ReceitaData {
+  cnpj: string;
+  nome_fantasia: string;
+  descricao_tipo_de_logradouro: string;
+  logradouro: string;
+  numero: string;
+  complemento: string;
+  bairro: string;
+  ddd_telefone_1: string;
+}
+
 export default function BairroPage() {
   const params = useParams();
   const estado = params?.estado;
   let cidade = params?.cidade;
   let bairro = params?.bairro;
+
+  const [receitaData, setReceitaData] = useState<ReceitaData[]>([]);
 
   if (typeof cidade === 'string') {
     cidade = cidade.replace(/_/g, ' ');
@@ -28,34 +43,66 @@ export default function BairroPage() {
     bairro = bairro.split('_').join(' ');
   }
 
-  // Verifique se os parâmetros 'estado', 'cidade' e 'bairro' estão definidos e são strings
   if (!estado || typeof estado !== 'string' ||
       !cidade || typeof cidade !== 'string' ||
       !bairro || typeof bairro !== 'string') {
     return <p>Parâmetros de estado, cidade ou bairro não fornecidos</p>;
   }
 
-  // Filtrar os dados com base no estado, cidade e bairro fornecidos
   const filteredData = (dados as Dado[]).filter(dado => dado.UF === estado && dado.MUNICÍPIO === cidade && dado.BAIRRO === bairro);
+  const cnpjs = filteredData.map(dado => dado.CNPJ);
 
-  // Remover duplicações de farmácias usando um Set
-  let uniquePharmacies = Array.from(new Set(filteredData.map(data => data.FARMÁCIA)));
+  const fetchReceitaData = async (cnpj: string) => {
+    try {
+      const response = await axios.get(`https://minhareceita.org/${cnpj}`);
+      return response.data;
+    } catch (error) {
+      console.error(`Erro ao buscar dados para o CNPJ ${cnpj}:`, error);
+      return null;
+    }
+  };
+
+  useEffect(() => {
+    const fetchAllData = async () => {
+      try {
+        const results = await Promise.all(cnpjs.map(cnpj => fetchReceitaData(cnpj)));
+        const validResults = results.filter(data => data !== null);
+
+        const uniqueResults = Array.from(new Set(validResults.map(data => data.nome_fantasia)))
+          .map(nome_fantasia => {
+            return validResults.find(data => data.nome_fantasia === nome_fantasia);
+          });
+
+        setReceitaData(uniqueResults);
+      } catch (error) {
+        console.error("Erro ao buscar dados:", error);
+      }
+    };
+
+    fetchAllData();
+  }, [cnpjs]);
+
+  const uniquePharmacies = Array.from(new Set(filteredData.map(data => data.FARMÁCIA)));
 
   return (
     <main>
       <h1>{bairro}, {cidade}, {estado}</h1>
       
       <ul>
-        {uniquePharmacies.map((farmacia, index) => (
-          <li key={index}>
-            <Link 
-              href={`/estados/${encodeURIComponent(estado)}/cidades/${(params.cidade ?? '')}/bairros/${encodeURIComponent(bairro ?? '')}/farmacias/${encodeURIComponent(farmacia.replace(/\s/g, '_'))}`} 
-              className="btnNeigborhoods"
-            >
-              <p><strong>Farmácia:</strong> {farmacia}</p>
-            </Link>
-          </li>
-        ))}
+        {receitaData.length > 0 ? (
+          receitaData.map((data, index) => (
+            <li key={data.cnpj || index}>
+              <Link 
+                href={`/estados/${encodeURIComponent(estado)}/cidades/${(params.cidade ?? '')}/bairros/${encodeURIComponent(bairro ?? '')}/farmacias/${encodeURIComponent(uniquePharmacies[index].replace(/\s/g, '_'))}`} 
+                className="btnNeigborhoods"
+              >
+                <p><strong>Farmácia:</strong> {data.nome_fantasia}</p>
+              </Link>
+            </li>
+          ))
+        ) : (
+          <p>Carregando...</p>
+        )}
       </ul>
     </main>
   );
